fb71385b6c29270fedf8d5913f734841fea6f197
Joaquin Di Lorenzo <joaquindilo2684@gmail.com>
2024-04-16 19:23:33 -0300

    Practica 1 y 2

---
+++ src/tp2/ej6/Tranformacion.java
@@ -0,0 +1,25 @@
+package tp2.ej6;
+
+import tp2.ej1.BinaryTree;
+
+public class Tranformacion {
+
+	private BinaryTree<Integer> arbol;
+	
+	
+	public Tranformacion(BinaryTree<Integer> arbol) {
+		this.arbol = arbol;
+	}
+
+	public BinaryTree<Integer> suma() {
+		BinaryTree<Integer> a = new BinaryTree<Integer>();
+		
+		return a;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
